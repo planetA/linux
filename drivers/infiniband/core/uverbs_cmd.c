b3cc0bade6ace0ad0e8b1b26fa263b571c1954c0
Ilya Meignan--Masson <ilya.meignan-masson@mailoo.org>
2023-06-28 18:09:00 +0200

    debug

--- drivers/infiniband/core/uverbs_cmd.c
+++ drivers/infiniband/core/uverbs_cmd.c
@@ -2024,6 +2024,7 @@ static int ib_uverbs_post_send(struct uverbs_attr_bundle *attrs)
 			if(!ud_onstack_used){
 				ud = &onstack_ud_wr;
 			}else{
+				pr_info("Allocating");
 				ud = alloc_wr(next_size, user_wr->num_sge);
 				if (!ud) {
 					ret = -ENOMEM;
@@ -2034,7 +2035,7 @@ static int ib_uverbs_post_send(struct uverbs_attr_bundle *attrs)
 			ud->ah = uobj_get_obj_read(ah, UVERBS_OBJECT_AH,
 						   user_wr->wr.ud.ah, attrs);
 			if (!ud->ah) {
-				if(ud_onstack_used)
+				if(ud_onstack_used && ud != &onstack_ud_wr)
 					kfree(ud);
 				ret = -EINVAL;
 				goto out_put;
@@ -2054,6 +2055,7 @@ static int ib_uverbs_post_send(struct uverbs_attr_bundle *attrs)
 			if(!rdma_onstack_used){
 				rdma = &onstack_rdma_wr;
 			}else{
+				pr_info("Allocating");
 				rdma = alloc_wr(next_size, user_wr->num_sge);
 				if (!rdma) {
 					ret = -ENOMEM;
@@ -2075,6 +2077,7 @@ static int ib_uverbs_post_send(struct uverbs_attr_bundle *attrs)
 			if(!atomic_onstack_used){
 				atomic = &onstack_atomic_wr;
 			}else{
+				pr_info("Allocating");
 				atomic = alloc_wr(next_size, user_wr->num_sge);
 				if (!atomic) {
 					ret = -ENOMEM;
@@ -2098,6 +2101,7 @@ static int ib_uverbs_post_send(struct uverbs_attr_bundle *attrs)
 			if(!next_onstack_used){
 				next = &next_onstack;
 			}else{
+				pr_info("Allocating");
 				next = alloc_wr(next_size, user_wr->num_sge);
 				if (!next) {
 					ret = -ENOMEM;
@@ -2161,6 +2165,7 @@ out_put:
 			uobj_put_obj_read(ud_wr(wr)->ah);
 		next = wr->next;
 		if(wr != &next_onstack && wr != &(onstack_rdma_wr.wr) && wr != &(onstack_ud_wr.wr) && wr != &(onstack_atomic_wr.wr)){
+			pr_info("freeing some memory");
 			kfree(wr);
 		}
 		wr = next;
@@ -2214,6 +2219,7 @@ ib_uverbs_unmarshall_recv_fastcall(struct uverbs_req_iter *iter, u32 wr_count,
 		if(!onstack_recv_wr_used){
 			next = onstack_recv_wr;
 		}else{
+			pr_info("Allocating");
 			next = kmalloc(ALIGN(sizeof(*next), sizeof(struct ib_sge)) +
 				       user_wr->num_sge * sizeof(struct ib_sge),
 			       GFP_KERNEL);
