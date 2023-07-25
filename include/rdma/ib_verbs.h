1af14206d0a40c3d23d8ca0da91bc833260113db
EricMuenzberg <erimue@outlook.de>
2023-07-25 15:32:36 +0200

    removed name print

--- include/rdma/ib_verbs.h
+++ include/rdma/ib_verbs.h
@@ -3994,7 +3994,7 @@ static inline int ib_probe_cq(struct ib_cq *cq)
 	printk("cq->device = %p", cq->device);
 	printk("cq->device->ops = %p", &(cq->device->ops));
 	printk("device name = %p", &(cq->device->name));
-	printk("device name chars = %c%c%c%c%c%c", cq->device->name[0],cq->device->name[1],cq->device->name[2], cq->device->name[3], cq->device->name[4], cq->device->name[5]);
+	//printk("device name chars = %c%c%c%c%c%c", cq->device->name[0],cq->device->name[1],cq->device->name[2], cq->device->name[3], cq->device->name[4], cq->device->name[5]);
 	printk("probe_one = %p", cq->device->ops.probe_one);
     return cq->device->ops.probe_one(cq);
 }
