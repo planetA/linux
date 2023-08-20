ecb366c04940ae5ab45c06467a39e6ac836b88b8
EricMuenzberg <erimue@outlook.de>
2023-08-20 19:03:40 +0200

    next_cqe_sw again

--- drivers/infiniband/hw/mlx5/cq.c
+++ drivers/infiniband/hw/mlx5/cq.c
@@ -449,19 +449,19 @@ static void mlx5_ib_poll_sw_comp(struct mlx5_ib_cq *cq, int num_entries,
 	}
 }
 
-static void *probe_sw_cqe(struct mlx5_ib_cq *cq, int n)
-{
-	void *cqe = get_cqe(cq, n & cq->ibcq.cqe);
-	struct mlx5_cqe64 *cqe64;
+// static void *probe_sw_cqe(struct mlx5_ib_cq *cq, int n)
+// {
+// 	void *cqe = get_cqe(cq, n & cq->ibcq.cqe);
+// 	struct mlx5_cqe64 *cqe64;
 
-	cqe64 = (cq->mcq.cqe_sz == 64) ? cqe : cqe + 64;
+// 	cqe64 = (cq->mcq.cqe_sz == 64) ? cqe : cqe + 64;
 
-	if (likely(get_cqe_opcode(cqe64) != MLX5_CQE_INVALID)) {
-		return cqe;
-	} else {
-		return NULL;
-	}
-}
+// 	if (likely(get_cqe_opcode(cqe64) != MLX5_CQE_INVALID)) {
+// 		return cqe;
+// 	} else {
+// 		return NULL;
+// 	}
+// }
 
 int mlx5_probe_one(struct ib_cq *ibcq)
 {
@@ -470,7 +470,7 @@ int mlx5_probe_one(struct ib_cq *ibcq)
 	unsigned long flags;
 	cq = to_mcq(ibcq);
 	spin_lock_irqsave(&cq->lock, flags);
-	cqe = probe_sw_cqe(cq, cq->mcq.cons_index);
+	cqe = next_cqe_sw(cq);
 	spin_unlock_irqrestore(&cq->lock, flags);
 	if (!cqe)
 		return -EAGAIN;
