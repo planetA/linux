47538349fee638aacfe3712741f92cc021a335b6
EricMuenzberg <erimue@outlook.de>
2023-08-22 21:59:48 +0200

    correct trace

--- drivers/infiniband/core/uverbs_cmd.c
+++ drivers/infiniband/core/uverbs_cmd.c
@@ -1222,11 +1222,11 @@ static void ib_uverbs_try_yield(struct ib_cq* cq)
 	if (!ret){
 		trace_ib_uverbs_probe_before_yield_to(sched_next_cq->poll_item.ts->pid, cur_poll->ts->pid);
 		yield_to(sched_next_cq->poll_item.ts, false);
-		trace_ib_uverbs_probe_after_yield(cur_poll->ts->pid);
 	} else {
 		trace_ib_uverbs_probe_before_cond_resched();
 		cond_resched();
 	}
+	trace_ib_uverbs_probe_after_yield(cur_poll->ts->pid);
 
 	//TODO assert
 	// spin_lock_irq(poll_list_lock_cpu);
